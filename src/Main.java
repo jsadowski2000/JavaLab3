519fa8974246d56389eb3f390a43d10247fb5b2a
jsadowski2000 <jsadowski2000@gmail.com>
2022-01-19 19:24:37 +0100

    Create .gitignore

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,32 @@ public class Main {
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
          */
+        List<Student> ListaStudentow = new ArrayList<>();
+        Student student1 = new Student();
+        student1.name = "Jakub";
+        student1.surname = "Sadowski";
+        student1.age = 21;
+        student1.status = true;
+        ListaStudentow.add(student1);
+
+
+        Student student2 = new Student();
+        student2.name = "Marian";
+        student2.surname = "Pazdzioch";
+        student2.age = 29;
+        student2.status = false;
+        ListaStudentow.add(student2);
+
+
+        Student student3 = new Student();
+        student3.name = "Ferdek";
+        student3.surname = "Kiepski";
+        student3.age = 24;
+        student3.status = true;
+        ListaStudentow.add(student3);
+
+        for (Student i : ListaStudentow) {
+            System.out.println(i.toString());
+        }
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    String name;
+    String surname;
+    boolean status;
+    int age;
+    public String toString(){
+        return "Name: " + name + " Surname: " + surname + " Age: " + age + " Status: " + status;
+    }
+}
