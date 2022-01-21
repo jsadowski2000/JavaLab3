83f81f4b6627786aa87c0c82f4cec5c54c4923f1
jsadowski2000 <jsadowski2000@gmail.com>
2022-01-21 19:33:34 +0100

    3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import javax.xml.namespace.QName;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -22,14 +24,39 @@ public class Main {
         System.out.println("Ineks osoby: " + januszIndeks);
 
 
-
         Person jakub = new Person("Jakub", "Sadowski");
         EnumDepartment department = EnumDepartment.inf;
-        Student first = new Student(jakub, department);
-        System.out.println(first.toString());
+        Student student1 = new Student(jakub, department);
+        //System.out.println(student1.toString());
+        List<Student> ListaStudentow = new ArrayList<>();
+        Person jan = new Person("Jan", "Matejko");
+        EnumDepartment department1 = EnumDepartment.inf;
+        Student student2 = new Student(jan, department1);
+
+        Person ferdek = new Person("Ferdek", "Kiepski");
+        EnumDepartment department2 = EnumDepartment.mat;
+        Student student3 = new Student(ferdek, department2);
+
+        Person ryszard = new Person("Ryszard", "Dran");
+        EnumDepartment department3 = EnumDepartment.inf;
+        Student student4 = new Student(ryszard, department3);
+
+        Person marian = new Person("Marian", "Pazdzioch");
+        EnumDepartment department4 = EnumDepartment.fiz;
+        Student student5 = new Student(marian, department4);
+        ListaStudentow.add(student5);
+        ListaStudentow.add(student1);
+        ListaStudentow.add(student2);
+        ListaStudentow.add(student3);
+        ListaStudentow.add(student4);
+
+        for(Student student : ListaStudentow) {
+            System.out.println(student.toString());
+            }
+         }
+
     }
 
-        }
 
  /* zad.2
             a) Stworzyć klasę Student z polami prywatnymi
